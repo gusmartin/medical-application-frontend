0ea53735fdf9ef7f2c2214a9a561d14d2d3d34d7
EstebanHdz <esteban.hd10@gmail.com>
2018-11-10 13:39:04 -0600

    get patients
    ya obtiene los pacientes activos y funciona el login

--- medico/main.js
+++ medico/main.js
@@ -1,21 +1,39 @@
 
 function login(){
+  var user = $("#inputEmail").val();
+  var pswd = $("#inputPassword").val();
+  
     var settings = {
       "async": true,
       "crossDomain": true,
       "url": "http://172.20.10.8:5000/api/v1/doctors/search_doctors",
       "method": "POST",
       "headers": {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json; charset=UTF-8"
+
       },
       "processData": false,
-      "data": "{\n\t\"action\": \"LOGIN\",\n\t\"user\": \"esteban@doctor.com\",\n\t\"password\": \"password\"\n}\n"
+      "data": "{\n\t\"action\": \"LOGIN\",\n\t\"user\": \""+user+"\",\n\t\"password\": \""+pswd+"\"\n}\n"
     };
-    console.log(settings);
+    //console.log(settings);
+    alert(user+" "+pswd);
+
     $.ajax(settings).done(function (response) {
       console.log(response)
+      alert("response")
+      
+      //var jsond = JSON.parse(response);
+
+      alert(response['login'])
+
+      if(response['login'] == "false")
+        alert("INCORRECT");
+      else 
+        window.location.replace("C:/Users/Esteban/Documents/GitHub/medical-application-frontend/medico/main.html")
     });
 
  }
 
- login();
+ //login();
+
+
---
+++ medico/patient.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+    alert("ready");
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://172.20.10.8:5000/api/v1/patients/search_patients",
+        "method": "POST",
+        "headers": {
+          "Content-Type": "application/json",
+          "cache-control": "no-cache",
+          "Postman-Token": "5b75ed41-afbc-45a9-a436-9ca06d8389b2"
+        },
+        "processData": false,
+        "data": " {\"action\": \"GET\",\"user_type\": \"active\"}"
+      }
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        var html = "";
+
+        html += "<table class='table table-sm table-hover header-fixed'  id='usuarios-activos-listing'>";
+        html += "<thead class='thead-light'><tr><th class='nombre-col'>Nombre</th><th class='nss-col'>NSS</th><th class='poliza-col'>Poliza</th><th class='act1-col'>Acción 1</th><th class='act2-col'>Acción 2</th></tr></thead>";
+        
+        html += "<tbody class = ''>";
+        for (var i = 0; i < response.length; i++) {
+
+          var ssn = response[i].ss_num;
+          var nombre = response[i].name;
+          var poliza = response[i].ass_policy;
+
+          html += "<tr class='usuario-row' data-href='#'>";
+          html += "<td class='nombre-col'>"+nombre+"</td><td class='nss-col'>"+ssn+"</td><td class='poliza-col'>"+poliza+"</td><td class='act1-col'><a href='#'class='btn btn-outline-primary' role='button'>Ver Reporte</a></td><td class='act2-col'><a onclick='modify("+ssn+")' href='#'class='btn btn-outline-secondary' role='button'>Modificar Status</a></td>";
+          html += "</tr>";
+        }
+        
+        html += "</tbody>";
+        html += "</table>";
+        html += "</br></br></br>";
+        html += "</br></br></br>";
+
+        $("#usuariosActivosLista").append(html);
+      });
+  });
