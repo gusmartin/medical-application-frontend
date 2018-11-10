16f6fdebe8c12af5e2f1543614d46b24185dd5c6
ferdlgr <mfer.dlgr97@gmail.com>
2018-11-10 13:38:07 -0600

    added modify function
    updated patient status

---
+++ medico/js/main.js
@@ -0,0 +1,20 @@
+function modify(input)
+{
+  var id =input;
+  var stat=prompt("Ingrese el estatus del Paciente", "Activo/Inactivo");
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://172.20.10.8:5000/api/v1/patients/search_patients",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "processData": false,
+    "data": "{\n\t\"action\": \"UPDATE_STATUS\",\n\t\"status\": \""+stat+"\",\n\t\"ss_num\": \""+id+"\"\n}"
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
